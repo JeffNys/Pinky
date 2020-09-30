4ff4a521e0693a9b96c1e57e84590c948f90961c
TripRaph78 <tripard.raphael@gmail.com>
2020-09-30 16:43:08 +0200

    ajout formulaire de contact

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+include("_nav.php");
+?>
+<form action="cible.php" method="post">
+<h2 id="contact">Contactez-moi !</h2>
+        <p>Pour d'autres informations ou questions , je me ferais un plaisir de vous répondre .<p>
+        
+        <p><label for="emailadress">Votre email svp :</label><input type="text" name="emailadress" id="emailadress" size="27" placeholder="@">
+        <p>
+            <label for="mail">votre message :</label> <br />
+            <textarea name="sendmail" id="mail" rows="15" cols="100">
+            </textarea>
+        </p>
+      <input type="submit" value="Envoyer" />
+</form>
+
+<?php
+include("_footer.php");
+?>
