8aa8e8f49a28d969f9ae3843bd5d5c3d42605217
JeffNys <sxb.jeanfrancois.nys@gmail.com>
2020-09-30 13:52:32 +0200

    ajout des éléments de la page et fond noir du site

--- index.php
+++ index.php
@@ -8,7 +8,35 @@
   </head>
   <body>
 	<?php include("_nav.php"); ?>
-	<h1>Speedy - Pinky</h1>	
+	<h1>Speedy - Pinky</h1>
+	<!-- les blocs sont contenus dans des div avec classes bloc
+	ensuite les bloc sont decalé avec une pseudo-class :nth(2) -->
+	<div class="blocs">
+		<!-- ici le bloc présentation -->
+		<h2>Mes capacités</h2>
+			
+	</div>
+
+	<div class="blocs">
+		<!-- ici le bloc dernière expériences -->
+		<h2>Mon parcour</h2>
+		<p>Je suis connu de tous dans mon rôle dans Pac-Man (qui appartient à l'histoire),
+		vous pouvez découvrir mon parcour un peu plus détaillé, ainsi que mes rôles cachés 
+		dans les coulisses de ce jeu, c'est ici: </p>	
+	</div>
+
+	<div class="blocs">
+		<!-- ici le bloc testez moi, ou, me voir en action -->
+		<h2>Me voir en action</h2>
+		<img src="giphy.gif" >	
+	</div>
+
+	<div class="blocs">
+		<!-- ici le bloc contactez moi -->
+		<h2>Contactez moi</h2>
+		<p>Si ça vous a plu, et que vous voulez me proposer du travail, il vous suffit de me contacter
+		</p>
+	</div>				
 	<?php include("_footer.php"); ?>
   </body>
 </html>
