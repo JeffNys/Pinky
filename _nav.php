f56593c008fba56c34bf29eaa96fe834b753518d
Loic-MEHAL <mehal.loic@gmail.com>
2020-09-30 16:58:42 +0200

    ajout modifs du fichier style.css

--- _nav.php
+++ _nav.php
@@ -1,2 +1,8 @@
-<?php
-echo "menu de navigation dans _nav.php <br>";
+<nav id='navigation'>
+    <ul>
+      <li id='main'><a href='index.php'>#</a></li>
+      <li><a href='experience.php'>Expériences</a></li>
+      <li><a href='game.php'>Testez-moi</a></li>
+      <li><a href='contact.php'>Contact</a></li>
+    </ul>
+</nav>
---
+++ game.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Speedy-Pinky</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="style_game.css" type="text/css">
+  </head>
+  <body>
+    <?php include("_nav.php"); ?>
+    <h1>Testez-moi !</h1>
+
+    <div id="game_content">
+    </div>
+    <div id="pacman">
+    </div>
+    <script src="pacman.js">
+    </script>
+    <script src="modernizr-1.5.min.js">
+    </script>
+
+    <script>
+
+      var el = document.getElementById("pacman");
+
+      if (Modernizr.canvas && Modernizr.localstorage &&
+      Modernizr.audio && (Modernizr.audio.ogg || Modernizr.audio.mp3)) {
+        window.setTimeout(function () { PACMAN.init(el, "./"); }, 0);
+      } else {
+        el.innerHTML = "Sorry, needs a decent browser<br /><small>" +
+          "(firefox 3.6+, Chrome 4+, Opera 10+ and Safari 4+)</small>";
+        }
+      </script>
+
+      <?php include("_footer.php"); ?>
+    </body>
+</html>
