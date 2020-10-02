ec595f0d5a910878eb542672c839af0eccab2ca2
Loic-MEHAL <mehal.loic@gmail.com>
2020-10-02 15:30:40 +0200

    Modifications visuelle du css général

--- _nav.php
+++ _nav.php
@@ -1,6 +1,6 @@
 <nav id='navigation'>
     <ul>
-      <li id='main'><a href='index.php'>#</a></li>
+      <li id='main'><a href='index.php'><img src = "pinky_nav.png" class = "pinkyNav"></a></li>
       <li><a href='experience.php'>Expériences</a></li>
       <li><a href='game.php'>Testez-moi</a></li>
       <li><a href='contact.php'>Contact</a></li>
