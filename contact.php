03f38b08c85e3f778d52389d31b0267b7bc7d849
TripRaph78 <tripard.raphael@gmail.com>
2020-10-02 11:53:36 +0200

    maj raph

--- contact.php
+++ contact.php
@@ -1,10 +1,9 @@
-<?php ?>
 <!DOCTYPE html>
 <html>
   <head>
     <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
-    <title>title</title>
+    <title>Speedy-Pinky</title>
   </head>
   <body>
  <?php 
@@ -12,19 +11,20 @@
 
 include("_nav.php");
 ?>
-<form action="cible.php" method="POST">
-<h2 id="contact">Contactez-moi !</h2>
-        <p>Pour d'autres informations ou questions , je me ferais un plaisir de vous répondre .<p>
-        <p><label for="Nom">Votre Nom svp :</label><input type="text" name="Nom" id="Nom" size="15">
+<h1 id="contact">Contactez-moi !</h1>
 
-        <p><label for="Nom">Votre Prénom svp :</label><input type="text" name="Prénom" id="Nom" size="15">
-        
-        <p><label for="emailadress">Votre email svp :</label><input type="text" name="emailadress" id="emailadress" size="27" placeholder="@">
-        <p>
-            <label for="mail">votre message :</label> <br />
-            <textarea name="sendmail" id="mail" rows="15" cols="100">
-            </textarea>
-        </p>
+  <form action="cible.php" method="POST">
+    <p>Pour d'autres informations ou questions , je me ferais un plaisir de vous répondre .<p>
+    <p><label for="Nom">Votre Nom svp :</label><input type="text" name="Nom" id="Nom" size="15">
+
+    <p><label for="Nom">Votre Prénom svp :</label><input type="text" name="Prénom" id="Nom" size="15">
+    
+    <p><label for="emailadress">Votre email svp :</label><input type="text" name="emailadress" id="emailadress" size="27" placeholder="@">
+    <p>
+        <label for="mail">votre message :</label> <br />
+        <textarea name="sendmail" id="mail" rows="15" cols="100">
+        </textarea>
+    </p>
       <input type="submit" value="Envoyer" />
 </form>
 
