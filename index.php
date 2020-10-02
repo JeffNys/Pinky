945a722b0aec60b32d3706f655aa8139f32ce8a0
JeffNys <sxb.jeanfrancois.nys@gmail.com>
2020-10-02 09:18:37 +0200

    mise en place des blocs décalés

--- index.php
+++ index.php
@@ -11,27 +11,28 @@
 	<h1>Speedy - Pinky</h1>
 	<!-- les blocs sont contenus dans des div avec classes blocsDroit et BlocsGauche -->
 	<div class="tram">
-		<div class="blocsGauche">
+		<div class="blocs">
 			<!-- ici le bloc présentation -->
 			<h2>Mes capacités</h2>
 			
 		</div>
 
-		<div class="blocsDroit">
+		<div class="blocs">
 			<!-- ici le bloc dernière expériences -->
-			<h2>Mon parcour</h2>
+			<h2>Mon parcours</h2>
 			<p>Je suis connu de tous dans mon rôle dans Pac-Man (qui appartient à l'histoire),
 			vous pouvez découvrir mon parcour un peu plus détaillé, ainsi que mes rôles cachés 
-			dans les coulisses de ce jeu, c'est ici: </p>	
+			dans les coulisses de ce jeu, c'est ici: </p>
+			<a href="experience.php" class="bouton1">Voir +</a>	
 		</div>
 		
-		<div class="blocsGauche">
+		<div class="blocs">
 			<!-- ici le bloc testez moi, ou, me voir en action -->
 			<h2>Me voir en action</h2>
 			<img src="giphy.gif" alt="animation de Pinky attrapant pacman">	
 		</div>
 		
-		<div class="blocsDroit">
+		<div class="blocs">
 			<!-- ici le bloc contactez moi -->
 			<h2>Contactez moi</h2>
 			<p>Si ça vous a plu, et que vous voulez me proposer du travail, il vous suffit de me contacter
