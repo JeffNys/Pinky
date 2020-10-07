c1a3ee78a0cd9913346efbd472bcbd806c5a3f7e
JeffNys <sxb.jeanfrancois.nys@gmail.com>
2020-10-07 14:16:49 +0200

    bords et fonds des fantomes

--- index.php
+++ index.php
@@ -8,50 +8,50 @@
   </head>
 
   <body>
-    <header>
-	<?php include("_nav.php"); ?>
-    </header>
-    <main>	
-    <h1>Speedy - Pinky</h1>
-	<!-- les blocs sont contenus dans des div avec classes blocsDroit et BlocsGauche -->
-	<div class="tram">
-		<div class="blocs">
-			<!-- ici le bloc présentation -->
-			<h2>Mes capacités</h2>
-			<ul>
-				<li>chasseuse d'intrus</li>
-				<li>coach en tracking</li>
-				<li>maitre d'armes</li>
-				<li>membre de la ligue des fantomes historique</li>
-				<li>liste à completer + mise en forme</li>
-			</ul>	
-		</div>
+    <div class="ecran">
+	    <?php include("_nav.php"); ?>
+        <main>	
+        <h1>Speedy - Pinky</h1>
+	    <!-- les blocs sont contenus dans des div avec classes blocsDroit et BlocsGauche -->
+	    <div class="tram">
+		    <div class="blocs">
+			    <!-- ici le bloc présentation -->
+			    <h2>Mes capacites</h2>
+			    <ul>
+				    <li>chasseuse d'intrus</li>
+				    <li>coach en tracking</li>
+				    <li>maitre d'armes</li>
+				    <li>membre de la ligue des fantomes historique</li>
+				    <li>liste à completer + mise en forme</li>
+			    </ul>	
+		    </div>
 
-		<div class="blocs">
-			<!-- ici le bloc dernière expériences -->
-			<h2>Mon parcours</h2>
-			<p>Je suis connu de tous dans mon rôle dans Pac-Man (qui appartient à l'histoire),
-			vous pouvez découvrir mon parcour un peu plus détaillé, ainsi que mes rôles cachés 
-			dans les coulisses de ce jeu, c'est ici: </p>
-			<a href="experience.php" class="bouton1">Voir +</a>	
-		</div>
-		
-		<div class="blocs">
-			<!-- ici le bloc testez moi, ou, me voir en action -->
-			<h2>Me voir en action</h2>
-			<img class="anime" src="giphy.gif" alt="animation de Pinky attrapant pacman">
-			<a href="game.php" class="bouton1">Testez-moi</a>
-		</div>
-		
-		<div class="blocs">
-			<!-- ici le bloc contactez moi -->
-			<h2>Contactez moi</h2>
-			<p>Si ça vous a plu, et que vous voulez me proposer du travail, il vous suffit de me contacter
-			</p>
-			<a href="contact.php" class="bouton1">contactez-moi</a>
-		</div>
-	</div>
-    </main>			
-	<?php include("_footer.php"); ?>
+		    <div class="blocs">
+			    <!-- ici le bloc dernière expériences -->
+			    <h2>Mon parcours</h2>
+			    <p>Je suis connu de tous dans mon role dans Pac-Man (qui appartient a l'histoire),
+			    vous pouvez decouvrir mon parcour un peu plus detaille, ainsi que mes roles caches 
+			    dans les coulisses de ce jeu, c'est ici: </p>
+			    <a href="experience.php" class="bouton1">Voir +</a>	
+		    </div>
+		    
+		    <div class="blocs">
+			    <!-- ici le bloc testez moi, ou, me voir en action -->
+			    <h2>Me voir en action</h2>
+			    <img class="anime" src="giphy.gif" alt="animation de Pinky attrapant pacman">
+			    <a href="game.php" class="bouton1">Testez-moi</a>
+		    </div>
+		    
+		    <div class="blocs">
+			    <!-- ici le bloc contactez moi -->
+			    <h2>Contactez moi</h2>
+			    <p>Si ca vous a plu, et que vous voulez me proposer du travail, il vous suffit de me contacter
+			    </p>
+			    <a href="contact.php" class="bouton1">contactez-moi</a>
+		    </div>
+	    </div>
+        </main>			
+	    <?php include("_footer.php"); ?>
+    </div>
   </body>
 </html>
