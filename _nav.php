5ae558ce11c91785825f776011b8c279100996d3
JeffNys <sxb.jeanfrancois.nys@gmail.com>
2020-10-09 10:14:58 +0200

    menu semi responsive

--- _nav.php
+++ _nav.php
@@ -2,35 +2,40 @@
     <ul>
         <li>
             <a href='index.php'>
-                <span class="parent"><img src = "img/Pinky.png" class = "pinkyNav">
-                    <span class="enfant">Acceuil
-                    </span>
-                </span>
-            </a>
+                <img src = "img/Pinky.png" class = "pinkyNav"><br>Acceuil </a>
         </li>
         <li>
             <a href='experience.php'>
-                <span class="parent"><img src = "img/Blinky.png" class = "pinkyNav">
-                    <span class="enfant">Experiences
-                    </span>
-                </span>
-            </a>
+                <img src = "img/Blinky.png" class = "pinkyNav"><br>Experiences</a>
         </li>
         <li>
             <a href='game.php'>
-                <span class="parent"><img src = "img/Inky.png" class = "pinkyNav">
-                    <span class="enfant">Testez-moi
-                    </span>
-                </span>
-            </a>
+               <img src = "img/Inky.png" class = "pinkyNav"><br>Testez-moi</a>
         </li>
         <li>
             <a href='contact.php'>
-                <span class="parent"><img src = "img/Clyde.png" class = "pinkyNav">
-                    <span class="enfant">Contactez-moi
-                    </span>
-                </span>
-            </a>
+                <img src = "img/Clyde.png" class = "pinkyNav"><br>Contactez-moi</a>
+        </li>
+    </ul>
+</nav>
+
+<nav id='navmobile'>
+    <ul>
+        <li>
+            <a href='index.php'>
+                <img src = "img/Pinky.png" class = "pinkyNav">Acceuil </a><br>
+        </li>
+        <li>
+            <a href='experience.php'>
+                <img src = "img/Blinky.png" class = "pinkyNav">Experiences</a><br>
+        </li>
+        <li>
+            <a href='game.php'>
+               <img src = "img/Inky.png" class = "pinkyNav">Testez-moi</a><br>
+        </li>
+        <li>
+            <a href='contact.php'>
+                <img src = "img/Clyde.png" class = "pinkyNav">Contactez-moi</a><br>
         </li>
     </ul>
 </nav>
